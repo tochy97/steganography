388c846573d27a3eca935cbb4d7e66c50be86e12
Chytoe <tochyegeonu97@gmail.com>
2022-04-20 12:11:28 -0500

    added user input for start and increment

--- hide_me.py
+++ hide_me.py
@@ -7,8 +7,11 @@ from tkinter.messagebox import showinfo
 from functools import partial
 
 root = Tk()
+
 frm = ttk.Frame(root, padding=10)
-frm.grid()
+frm.pack()
+
+entry = ttk.Frame(root, padding=10)
 
 
 class FileComp:
@@ -44,14 +47,13 @@ def select_a_file():
         return None
 
 
-
-def hide_secret(secret, container, start, inbetween):
+def hide_secret(secret, container, start, increment):
     # where the message hiding happens
 
     stop = start
     inner_stop = 0
     # holds new byte array
-    output = b""+start.to_bytes(1, "big")+inbetween.to_bytes(1, "big")
+    output = b""+start.to_bytes(1, "big")+increment.to_bytes(1, "big")
     # count will keep track of how many we have skipped
     count = 0
     iteration = 0
@@ -59,8 +61,8 @@ def hide_secret(secret, container, start, inbetween):
         count += 1
 
         # we will only be replacing bits of the start byte of the container
-        # and every bit that is inbetween distance away
-        if((count == start) | ((count > stop) & (count == (stop + inbetween)))):
+        # and every bit that is increment distance away
+        if((count == start) | ((count > stop) & (count == (stop + increment)))):
 
             # stop keeps track of where we stopped in container byte steam
             stop = count
@@ -84,7 +86,27 @@ def hide_secret(secret, container, start, inbetween):
     return output
 
 
-def hide_a_message():
+start_label = Label(
+    entry, 
+    text="Enter starting index", 
+    font=('Calibri 10')
+    )
+start_box = Entry(
+    entry, 
+    width=35
+    )
+
+increment_label = Label(
+    entry, 
+    text="Enter increment value", 
+    font=('Calibri 10')
+    )
+increment_box = Entry(
+    entry, 
+    width=35
+    )
+
+def hide_a_message(start, increment):
     # choose the  file you want to hide and the container file
 
     first = select_a_file()
@@ -102,27 +124,66 @@ def hide_a_message():
     if second == None:
         return
 
-    hidden_data = hide_secret(secret=first.data, container=second.data, start=10, inbetween=5)
-
+    try:
+        hidden_data = hide_secret(
+            secret=first.data, 
+            container=second.data, 
+            start=int(start),
+            increment=int(increment)
+        )
+    except:
+        return showinfo(
+            title="Error",
+            message="Failed to hide, values too large"
+        )
+    
     save_file = fd.asksaveasfile(mode='wb')
 
     if save_file == None:
         return
     else:
         save_file.write(hidden_data)
+        showinfo(
+            title="Success",
+            message="Message hidden!"
+        )
+    start_label.pack_forget()
+    start_box.pack_forget()
+    increment_label.pack_forget()
+    increment_box.pack_forget()
+    entry.pack_forget()
+    increment_box.pack
+    return frm.pack()
+
+
+def get_start_increment():
+    frm.pack_forget()
+    entry.pack()
+    start_label.pack()
+    start_box.pack()
+    increment_label.pack()
+    increment_box.pack()
+    hide_me = ttk.Button(
+        entry,
+        text='Select file to hide',
+        command=lambda: [hide_a_message(
+            start=start_box.get(), 
+            increment=increment_box.get()
+            )]
+    ).pack()
+
 
 
 create_secret = ttk.Button(
     frm,
-    text='Select file to hide',
-    command=lambda: [hide_a_message()]
-)
+    text='Hide secret in file',
+    command=lambda: [get_start_increment()]
+).pack()
 
 
 def get_secret(message):
-    print("before ", message)
     start = 0
-    inbetween = 0
+    increment = 0
     output = b""
 
     # count will keep track of how many we have skipped
@@ -132,50 +193,59 @@ def get_secret(message):
         if(count == 0):
             start = message_block
         if(count == 1):
-            inbetween = message_block
+            increment = message_block
         count += 1
     end = message[-1]
     count = 0
     iteration = 0
     start += 2
     stop = start
-    print(start, inbetween, end," hello")
     for message_black in message:
         count += 1
-        if((count == start) | ((count > stop) & (count == (stop + inbetween))) & (iteration < end)):
+        if((count == start) | ((count > stop) & (count == (stop + increment))) & (iteration < end)):
             iteration += 1
             stop = count
             output += message_black.to_bytes(1, "big")
-    print(iteration,end, "after ",output)
     return output
 
 def get_that_secret():
     message = select_a_file()
     if(message == None):
         return
-    get_secret(message.data)
+    try:
+        hidden_data = get_secret(message.data)
+    except:
+        showinfo(
+            title="Error",
+            message="Failed to hide"
+        )
+    save_file = fd.asksaveasfile(mode='wb')
+
+    if save_file == None:
+        return
+    else:
+        save_file.write(hidden_data)
+        showinfo(
+            title="Success",
+            message="Message hidden!"
+        )
 
 
 def print_byte_data():
     message = select_a_file()
-    print(message.data)
-    
 
 
 print_byte = ttk.Button(
     frm,
     text='Print byte data',
     command=lambda: [print_byte_data()]
-)        
+).pack()        
 
 extract_secret = ttk.Button(
     frm,
     text='Select file with a secret',
     command=lambda: [get_that_secret()]
-)
+).pack()
 
 # all the home screen buttons
-create_secret.pack()
-extract_secret.pack()
-print_byte.pack()
 root.mainloop()
